965dde981f3eba234941b6a0bd572e7b2c469d55
Edward Pie <hackstockpie@gmail.com>
2020-08-31 14:40:51 +0200

    adds keyboard teleoperation support

---
+++ src/apps/keyboard_teleop.py
@@ -0,0 +1,23 @@
+import curses
+
+from robot.tanks import DifferentialDriveTank
+
+def main(stdscr):
+    curses.curs_set(0)
+    tank = DifferentialDriveTank()
+
+    while True:
+        key = stdscr.getch()
+
+        if key == curses.KEY_UP:
+            tank.move(50)
+        elif key == curses.KEY_DOWN:
+            tank.move(-50)
+        elif key == curses.KEY_LEFT:
+            tank.spin(-50,50)
+        elif key == curses.KEY_RIGHT:
+            tank.spin(-50,50)
+        elif key == curses.KEY_ENTER  or key in [10,13]:
+            tank.stop()
+
+curses.wrapper(main)
