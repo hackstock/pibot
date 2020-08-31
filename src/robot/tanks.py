9b35f644c663c63daf4718a811c9ab6f17e223d8
Edward Pie <hackstockpie@gmail.com>
2020-08-31 12:51:27 +0200

    fixes differential drive tank

--- src/robot/tanks.py
+++ src/robot/tanks.py
@@ -2,12 +2,12 @@ from robot_base import PWMTank
 
 class DifferentialDriveTank(object):
 
-    def __init__(self, rm_fw_pin=9, rm_bw_pin=10, lm_fw_pin=8, lm_fw_pin=7):
+    def __init__(self, rm_fw_pin=9, rm_bw_pin=10, lm_fw_pin=8, lm_bw_pin=7):
         self.tank = PWMTank(
-            lm_fw_pin=lm_fw_pin,
-            lm_bw_pin=lm_bw_pin,
             rm_fw_pin=rm_fw_pin,
-            rm_bw_pin=rm_bw_pin
+            rm_bw_pin=rm_bw_pin,
+            lm_fw_pin=lm_fw_pin,
+            lm_bw_pin=lm_bw_pin
         )
 
     def move(self, speed):
