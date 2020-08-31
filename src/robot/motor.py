293c80361f9c04b0ebfb691b8b0b6f6f760446a8
Edward Pie <hackstockpie@gmail.com>
2020-08-31 12:32:07 +0200

    renames motor pins and implements clipping of duty cycle

--- src/robot/motor.py
+++ src/robot/motor.py
@@ -2,39 +2,46 @@ import RPi.GPIO as GPIO
 
 class PWMMotor(object):
 
-    def __init__(self, name, pin_one, pin_two, pwm_freq=1000):
+    def __init__(self, name, forward_pin, backward_pin, pwm_freq=1000):
 
-        GPIO.cleanup()
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
 
         self.name = name
-        self.pin_one = pin_one
-        self.pin_two = pin_two
+        self.forward_pin = forward_pin
+        self.backward_pin = backward_pin
 
-        GPIO.setup(self.pin_one, GPIO.OUT)
-        GPIO.setup(self.pin_two, GPIO.OUT)
+        GPIO.setup(self.forward_pin, GPIO.OUT)
+        GPIO.setup(self.backward_pin, GPIO.OUT)
 
-        self.pwm_one = GPIO.PWM(self.pin_one, pwm_freq)
-        self.pwm_two = GPIO.PWM(self.pin_two, pwm_freq)
+        self.forward_pwm = GPIO.PWM(self.forward_pin, pwm_freq)
+        self.backward_pwm = GPIO.PWM(self.backward_pin, pwm_freq)
 
     def _move_forward(self, duty_cycle):
-        self.pwm_two.stop()
-        self.pwm_one.start(duty_cycle)
+        self.backward_pwm.stop()
+        self.forward_pwm.start(duty_cycle)
 
     def _move_backward(self, duty_cycle):
-        self.pwm_one.stop()
-        self.pwm_two.start(duty_cycle)
+        self.forward_pwm.stop()
+        self.backward_pwm.start(duty_cycle)
+
+    def _clip_speed(self, speed):
+        if speed < -100:
+            return -100
+        elif speed > 100:
+            return 100
+        else:
+            return speed
 
     def move(self, speed):
-        duty_cycle = abs(speed)
+        duty_cycle = abs(self._clip_speed(speed))
         if speed < 0:
             self._move_backward(duty_cycle)
         else:
             self._move_forward(duty_cycle)
 
     def stop(self):
-        self.pwm_one.stop()
-        self.pwm_two.stop()
+        self.forward_pwm.stop()
+        self.backward_pwm.stop()
 
 
