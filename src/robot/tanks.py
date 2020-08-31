aeb86c183227608e91243df156df09c32aa2429d
Edward Pie <hackstockpie@gmail.com>
2020-08-31 15:14:59 +0200

    quick fix

--- src/robot/tanks.py
+++ src/robot/tanks.py
@@ -18,3 +18,25 @@ class DifferentialDriveTank(object):
 
     def stop(self):
         self.tank.stop()
+
+def main(stdscr):
+    curses.curs_set(0)
+    tank = DifferentialDriveTank()
+
+    while True:
+        key = stdscr.getch()
+
+        if key == curses.KEY_UP:
+            tank.move(50)
+        elif key == curses.KEY_DOWN:
+            tank.move(-50)
+        elif key == curses.KEY_LEFT:
+            tank.spin(-50,50)
+        elif key == curses.KEY_RIGHT:
+            tank.spin(-50,50)
+        elif key == curses.KEY_ENTER  or key in [10,13]:
+            tank.stop()
+
+
+if __name__ == "__main__":
+    curses.wrapper(main)
